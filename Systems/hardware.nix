655abb9c4200f60a0e83979a496836f23b079db5
spidyx5 <spidyx5@duck.com>
2025-12-03 02:02:54 +0600

    a

--- Systems/hardware.nix
+++ Systems/hardware.nix
@@ -40,17 +40,19 @@
       intel-compute-runtime   # OpenCL support for Intel GPUs
       vpl-gpu-rt             # Intel Video Processing Library
       intel-graphics-compiler
-      mesa_git
+      
     ];
 
     # 32-bit versions of the same drivers for compatibility.
     extraPackages32 = with pkgs; [
       intel-media-driver
       libva
-      #mesa32_git
     ];
   };
-
+  
+   package = pkgs.mesa_git; 
+   package32 = pkgs.mesa32_git;
+  
   # ENVIRONMENT VARIABLES FOR VIDEO ACCELERATION
   environment.sessionVariables = { 
     LIBVA_DRIVER_NAME = "iHD"; 
