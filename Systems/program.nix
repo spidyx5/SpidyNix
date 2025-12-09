2db48eccb7080a4f268def1dc73f513fd3a7b390
spidyx5 <spidyx5@duck.com>
2025-12-09 21:47:50 +0600

    feat(program): add niri package

--- Systems/program.nix
+++ Systems/program.nix
@@ -81,6 +81,7 @@
     #proton-cachyos_nightly_x86_64_v3
     twitch-hls-client
     obs-studio-plugins.obs-vkcapture
+    niri
 
     inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
     inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.default
