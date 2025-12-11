8f5c65d64868b6935484cbd5a47a244ecff0e4e6
spidyx5 <spidyx5@duck.com>
2025-12-12 05:50:42 +0600

    commit

--- Systems/nixos.nix
+++ Systems/nixos.nix
@@ -21,7 +21,6 @@
         "https://cache.nixos.org?priority=10"
         "https://chaotic-nyx.cachix.org"
         "https://nix-gaming.cachix.org"
-        "https://niri.cachix.org"
         "https://nix-community.cachix.org?priority=8"
       ];
 
@@ -29,7 +28,6 @@
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-        "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
